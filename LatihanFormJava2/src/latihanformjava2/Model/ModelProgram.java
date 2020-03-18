d53fc17874f37a821215f6213eb0ce92f8fb621c
azharkhairyl <61782676+azharkhairyl@users.noreply.github.com>
2020-03-18 17:22:47 +0700

    Add files via upload

---
+++ LatihanFormJava2/src/latihanformjava2/Model/ModelProgram.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanformjava2.Model;
+
+/**
+ *
+ * @author Legion Y520
+ */
+public class ModelProgram {
+    public String label = "Please put your information to get the result" ;
+    
+    public String returnGenderNoun(String gender){
+        String noun = "He";
+        if (gender.toLowerCase().equals("female")){
+    }
+        return noun;
+        
+    }
+    
+    public String returnMental (String mental){
+        switch(mental){
+            case ":)":
+                return "Smilling";
+            case ":D":
+                return "Happy";
+            case ":(":
+                return "Sad";
+            case ": '(":
+                return "Crying";
+            default:
+                return "In Love" ;
+        }
+    }
+    
+    
+}
+
+    
+    
